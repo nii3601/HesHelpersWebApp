3e954f61dba1daaeae0be6784cdf70de7571f7d5
Jonathan Lamptey <jlamptey@nd.edu>
2022-10-10 14:30:29 -0400

    created API REQUESTSA

---
+++ RobotDriveCode/APIRequests/API.py
@@ -0,0 +1,14 @@
+
+
+def checkAssignment(robotID=None):
+    '''
+        CHECKS IF ROBOT HAS BEEN ASSIGNED
+    '''
+    pass
+
+def getBookInfo(bookID=None):
+    '''
+        CHECK BOOK INFORMATION FROM DATABASE GIVEN BOOK ID
+    '''
+    pass
+
