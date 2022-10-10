58d0824cfe5ddb01764b4a665a803f94e6b381c5
Jonathan Lamptey <jlamptey@nd.edu>
2022-10-10 14:46:52 -0400

    Done with check assignment

--- RobotDriveCode/APIRequests/API.py
+++ RobotDriveCode/APIRequests/API.py
@@ -1,13 +1,31 @@
 import requests
 
-BASE_URL = "https://parseapi.back4app.com/classes/"
+BASE_URL = "https://parseapi.back4app.com/functions/"
 
 def checkAssignment(robotID=None):
     '''
         CHECKS IF ROBOT HAS BEEN ASSIGNED
+        RETURNS NONE OR BOOK ID
     '''
+    if(robotID == None):
+        return None
 
-    pass
+    function = "CheckBotAssignment" # define function
+
+    result = requests.get(
+        url=BASE_URL+function,
+        params={
+            'botName':robotID
+            }
+    )
+
+    data = result.json()
+
+    if(not data.Assigned):
+        return None
+    
+    return data.Destination.objectID
+
 
 def getBookInfo(bookID=None):
     '''
