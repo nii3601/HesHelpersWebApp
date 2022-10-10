a25ebf876d12b3c59114d7422bbf9568fec69155
Jonathan Lamptey <jlamptey@nd.edu>
2022-10-10 15:15:03 -0400

    Done with Main API Calls :

--- RobotDriveCode/APIRequests/API.py
+++ RobotDriveCode/APIRequests/API.py
@@ -1,6 +1,11 @@
 import requests
 
 BASE_URL = "https://parseapi.back4app.com/functions/"
+HEADERS = {
+    'X-Parse-Application-id': 'v840lhtRewsAyjbP5uC8DUgZ7lT1x5dwXdtbTJQs',
+    'X-Parse-REST-API-Key' : 'RCGvPiJOSQ6SWucyiLu0jzgGJpJKr1zeKn57bLtq',
+    'Content-type':'application/json',   
+}
 
 def checkAssignment(robotID=None):
     '''
@@ -12,24 +17,45 @@ def checkAssignment(robotID=None):
 
     function = "CheckBotAssignment" # define function
 
-    result = requests.get(
+    result = requests.post(
         url=BASE_URL+function,
         params={
             'botName':robotID
-            }
+            },
+        headers=HEADERS
     )
 
     data = result.json()
 
-    if(not data.Assigned):
+    if(not data['result']["Assigned"]):
         return None
     
-    return data.Destination.objectID
+    return getBookInfo(data['result']['Destination']['objectId'])
 
 
 def getBookInfo(bookID=None):
     '''
         CHECK BOOK INFORMATION FROM DATABASE GIVEN BOOK ID
     '''
-    pass
+    if(bookID == None):
+        return None
 
+    function = "GetBook" # define function
+
+    result = requests.post(
+        url=BASE_URL+function,
+        params={
+            'objectId':bookID
+            },
+        headers=HEADERS
+    )
+
+    data = result.json()
+
+    if(len(data) == 0):
+        return None
+    
+    return data['result'][0]
+
+
+print(checkAssignment('Bob'))
