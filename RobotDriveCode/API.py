a7aee356dd0f46492d0297921959f7278e37c59d
Jonathan Lamptey <jlamptey@nd.edu>
2022-11-11 12:52:43 -0500

    testing

--- RobotDriveCode/API.py
+++ RobotDriveCode/API.py
@@ -1,8 +1,10 @@
 # import http.client
-import httplib
+# import httplib
 import json
+import urllib
 
-conn = httplib.HTTPSConnection("parseapi.back4app.com")
+
+# conn = httplib.HTTPSConnection("parseapi.back4app.com")
 payload = ''
 headers = {
       'X-Parse-Application-id': 'v840lhtRewsAyjbP5uC8DUgZ7lT1x5dwXdtbTJQs',
@@ -10,70 +12,78 @@ headers = {
       'Content-type': 'application/json'
     }
 
-def checkAssignment(robotID=None):
-    '''
-        CHECKS IF ROBOT HAS BEEN ASSIGNED
-        RETURNS NONE OR BOOK ID
-    '''
-    if(robotID == None):
-        return None
 
-    function = "CheckBotAssignment" # define function
+def test():
+    f = urllib.urlopen("https://parseapi.back4app.com/functions/CheckBotAssignment?botName=Bob")
+    print(f.read())
 
-    conn.request("POST", "/functions/CheckBotAssignment?botName="+robotID, payload, headers)
-    res = conn.getresponse()
-    data = res.read()
-    data = json.loads(data)
+test()
 
-    conn.close()
+# def checkAssignment(robotID=None):
+#     '''
+#         CHECKS IF ROBOT HAS BEEN ASSIGNED
+#         RETURNS NONE OR BOOK ID
+#     '''
 
-    if(not data['result']["Assigned"]):
-         return None
+#     if(robotID == None):
+#         return None
+
+#     function = "CheckBotAssignment" # define function
+
+#     conn.request("POST", "/functions/CheckBotAssignment?botName="+robotID, payload, headers)
+#     res = conn.getresponse()
+#     data = res.read()
+#     data = json.loads(data)
+
+#     conn.close()
+
+#     if(not data['result']["Assigned"]):
+#          return None
     
-    return getBookInfo(data['result']['Destination']['objectId'])
+#     return getBookInfo(data['result']['Destination']['objectId'])
 
 
-def getBookInfo(bookID=None):
-    '''
-        CHECK BOOK INFORMATION FROM DATABASE GIVEN BOOK ID
-    '''
-    if(bookID == None):
-        return None
+# def getBookInfo(bookID=None):
+#     '''
+#         CHECK BOOK INFORMATION FROM DATABASE GIVEN BOOK ID
+#     '''
+#     if(bookID == None):
+#         return None
 
-    function = "GetBook" # define function
+#     function = "GetBook" # define function
 
-    conn.request("POST", "/functions/GetBook?objectId="+bookID, payload, headers)
-    res = conn.getresponse()
-    data = res.read()
-    data = json.loads(data)
+#     conn.request("POST", "/functions/GetBook?objectId="+bookID, payload, headers)
+#     res = conn.getresponse()
+#     data = res.read()
+#     data = json.loads(data)
 
-    if(len(data) == 0):
-        return None
+#     if(len(data) == 0):
+#         return None
 
-    conn.close()
+#     conn.close()
     
-    return data['result'][0]
+#     return data['result'][0]
 
 
 
-def resetBot(botName=None):
-    '''
-        RESETS ROBOT STATUS
-    '''
-    if(botName == None):
-        return None
+# def resetBot(botName=None):
+#     '''
+#         RESETS ROBOT STATUS
+#     '''
+#     if(botName == None):
+#         return None
 
-    function = "ResetBot" # define function
+#     function = "ResetBot" # define function
 
-    conn.request("POST", "/functions/ResetBot?botName="+botName, payload, headers)
-    res = conn.getresponse()
-    data = res.read()
-    data = json.loads(data)
+#     conn.request("POST", "/functions/ResetBot?botName="+botName, payload, headers)
+#     res = conn.getresponse()
+#     data = res.read()
+#     data = json.loads(data)
 
-    if(not data['result']):
-        return None
+#     if(not data['result']):
+#         return None
 
-    conn.close()
+#     conn.close()
     
-    return data['result']
+#     return data['result']
 
