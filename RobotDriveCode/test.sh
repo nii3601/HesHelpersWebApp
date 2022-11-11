e69688e6da29ad7e0a2aba6e9cfeaa493f454358
Jonathan Lamptey <jlamptey@nd.edu>
2022-11-11 13:33:17 -0500

    trying with shell

---
+++ RobotDriveCode/test.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+curl --location --request POST 'https://parseapi.back4app.com/functions/CheckBotAssignment?botName=Bob' \
+--header 'X-Parse-Application-id: v840lhtRewsAyjbP5uC8DUgZ7lT1x5dwXdtbTJQs' \
+--header 'X-Parse-REST-API-Key: RCGvPiJOSQ6SWucyiLu0jzgGJpJKr1zeKn57bLtq' \
+--header 'Content-type: application/json'
